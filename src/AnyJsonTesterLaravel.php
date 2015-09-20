0f79d281c5c789c07a5758523203df9dd8c5ad52
Zloblin <vamakin@gmail.com>
2015-09-20 21:06:17 +0300

    trait for Laravel

---
+++ src/AnyJsonTesterLaravel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AnyJsonTester;
+
+
+use AnyJsonTester\Types\AnyArray;
+use AnyJsonTester\Types\AnyObject;
+
+trait AnyJsonTesterLaravel {
+
+    /**
+     * Assert that json matches pattern
+     * @param AnyObject|AnyArray $expected
+     * @param bool $negate
+     * @return $this
+     */
+    public function seeJsonLike($expected, $negate = false )
+    {
+        $method = $negate ? 'assertFalse' : 'assertTrue';
+        $actual = json_decode($this->response->getContent(), true);
+        $checkResult = $expected->check($actual);
+        $this->$method( $checkResult['passed'], $checkResult['message'] );
+        return $this;
+    }
+
+}
