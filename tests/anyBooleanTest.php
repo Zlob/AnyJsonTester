f3c9a3e3ffd553ba72fb902bba4f0b9639cae6a1
Zloblin <vamakin@gmail.com>
2015-09-20 14:46:52 +0300

    anyBoolean tests

---
+++ tests/anyBooleanTest.php
@@ -0,0 +1,65 @@
+<?php
+
+
+class AnyBooleanTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider booleanDataProviderPassed
+     */
+    public function testAnyBooleanPassed($actualArray, $strict = false, $nullable = false)
+    {
+        $expected = new \AnyJsonTester\Types\AnyBoolean($strict, $nullable);
+        $checkResult = $expected->check($actualArray);
+        static::assertTrue($checkResult['passed'], $checkResult['message']);
+    }
+
+    public function booleanDataProviderPassed()
+    {
+        return [
+            ['0', false, false],
+            ['1', false, false],
+
+            ['off', false, false],
+            ['on', false, false],
+
+            ['no', false, false],
+            ['yes', false, false],
+
+            ['true', false, false],
+            ['false', false, false],
+
+            ['true', true, false],
+            ['false', true, false],
+
+            [null, false, true],
+        ];
+    }
+
+    /**
+     * @dataProvider booleanDataProviderFailed
+     */
+    public function testAnyBooleanFailed($actualArray, $strict = false, $nullable = false)
+    {
+        $expected = new \AnyJsonTester\Types\AnyBoolean($strict, $nullable);
+        $checkResult = $expected->check($actualArray);
+        static::assertFalse($checkResult['passed'], $checkResult['message']);
+    }
+
+    public function booleanDataProviderFailed()
+    {
+        return [
+            ['0', true, false],
+            ['1', true, false],
+
+            ['off', true, false],
+            ['on', true, false],
+
+            ['no', true, false],
+            ['yes', true, false],
+
+            [null, false, false],
+        ];
+    }
+
+}
